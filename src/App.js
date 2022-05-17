7390f0f943bf02cbf9fe66a69444663244702958
Ruben Poelen <ruben.poelen@hotmail.com>
2022-05-17 15:22:31 +0200

    initial change and setup

--- src/App.js
+++ src/App.js
@@ -10,29 +10,15 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
-
+import Login from './components/Login/Login';
+import Signup from './components/Sign-up/Signup';
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+          <Login />
         </Grid>
       </Box>
     </ChakraProvider>
---
+++ src/components/Sign-up/Signup.jsx
@@ -0,0 +1,147 @@
+import {
+  Icon,
+  Button,
+  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+  useBreakpointValue,
+  Box,
+  InputGroup,
+  InputRightElement,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import * as React from 'react';
+import { GoogleIcon } from './ProviderIcons';
+import { GitHubIcon } from './ProviderIcons';
+import { FaPlaneDeparture } from 'react-icons/fa';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+const Signup = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  const handleUsername = e => setUsername(e.target.value);
+  const handleEmail = e => setEmail(e.target.value);
+  const handlePassword = e => setPassword(e.target.value);
+
+  const icon = useColorModeValue('gray.600', 'gray.600');
+  return (
+    <Container
+      maxW="md"
+      py={{
+        base: '12',
+        md: '24',
+      }}
+    >
+      <Stack spacing="8">
+        <Stack spacing="6" align="center">
+          <Icon as={FaPlaneDeparture} />
+          <Stack spacing="3" textAlign="center">
+            <Heading
+              size={useBreakpointValue({
+                base: 'xs',
+                md: 'sm',
+              })}
+            >
+              Create an account
+            </Heading>
+            <Text color="muted">Start making your dreams come true</Text>
+          </Stack>
+        </Stack>
+        <Stack spacing="6">
+          <Box as={'form'}>
+            <Stack spacing="5">
+              <FormControl isRequired>
+                <FormLabel htmlFor="name">Username</FormLabel>
+                <Input
+                  onChange={handleUsername}
+                  id="name"
+                  value={username}
+                  type="text"
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  onChange={handleEmail}
+                  id="email"
+                  value={email}
+                  type="email"
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    id="password"
+                    value={password}
+                    type={showPassword ? 'text' : 'password'}
+                    onChange={handlePassword}
+                  />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'primary'}
+                      onClick={() => {
+                        setShowPassword(showPassword => !showPassword);
+                      }}
+                    >
+                      {showPassword ? (
+                        <ViewIcon color={icon} />
+                      ) : (
+                        <ViewOffIcon color={icon} />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText color="muted">
+                  At least 8 characters long
+                </FormHelperText>
+              </FormControl>
+            </Stack>
+            <Stack spacing="4">
+              <Button
+                type="submit"
+                variant="primary"
+                _active={{ boxShadow: 'lg' }}
+                _hover={{ boxShadow: 'md' }}
+              >
+                Create account
+              </Button>
+              <Button
+                variant="secondary"
+                leftIcon={<GoogleIcon boxSize="5" />}
+                iconSpacing="3"
+              >
+                Sign up with Google
+              </Button>
+              <Button
+                variant="secondary"
+                leftIcon={<GitHubIcon boxSize="5" />}
+                iconSpacing="3"
+              >
+                Sign up with Github
+              </Button>
+            </Stack>
+          </Box>
+        </Stack>
+        <HStack justify="center" spacing="1">
+          <Text fontSize="sm" color="muted">
+            Already have an account?
+          </Text>
+          <Button variant="link" colorScheme="blue" size="sm">
+            Log in
+          </Button>
+        </HStack>
+      </Stack>
+    </Container>
+  );
+};
+export default Signup;
